73bd8f257dd41589fde2ebf7945531ffc35b5bd5
Jose Cambronero <jcamsan@mit.edu>
2019-01-23 20:01:44 +0000

    some more block reordering

--- src/experiments.py
+++ src/experiments.py
@@ -209,6 +209,139 @@ def encode_dataset(ds, code_path, nl_path, encoder_path, target_len):
         )
 
 
+def generate_experiments(
+        train_data_path,
+        test_data_path,
+        output_dir,
+        train_downsample=None,
+        seed=None,
+        force=False,
+):
+    """
+    Generates subfolders for experiments
+    """
+    with open(train_data_path, "rb") as fin:
+        train_data = pickle.load(fin)
+
+    with open(test_data_path, "rb") as fin:
+        test_data = pickle.load(fin)
+
+    utils.create_dir(output_dir)
+
+    experiments = paper_experiments(output_dir)
+
+    for exp in experiments:
+        print("Generating experiment: {}".format(exp["output_dir"]))
+        if os.path.exists(exp["output_dir"]) and not force:
+            print("Skipping {}, output folder exists".format(
+                exp["output_dir"]))
+            print("Use --force if re-run is desired")
+            continue
+
+        generate_experiment_folder(
+            train_data,
+            test_data,
+            code_pipeline=exp["code"],
+            nl_pipeline=exp["nl"],
+            code_test_pipeline=exp["code_test"],
+            nl_test_pipeline=exp["nl_test"],
+            min_vocab_count=exp["min_count"],
+            output_dir=exp["output_dir"],
+            downsample_train=exp["downsample_train"],
+            downsample_valid=exp["downsample_valid"],
+            seeds=exp.get("seeds", seed),
+        )
+
+
+def run_experiments(base_dir, force=False):
+    experiment_folders = [
+        os.path.join(base_dir, p) for p in os.listdir(base_dir)
+    ]
+    for experiment_root in experiment_folders:
+        # Embeddings, encoder, test and validation data are seed independent
+        embeddings_path = os.path.join(experiment_root, "embeddings.vec")
+        encoder_path = os.path.join(experiment_root, "encoder.pkl")
+
+        test_code_path = os.path.join(experiment_root, "test-code.npy")
+        test_nl_path = os.path.join(experiment_root, "test-nl.npy")
+
+        valid_code_path = os.path.join(experiment_root, "valid-code.npy")
+        valid_nl_path = os.path.join(experiment_root, "valid-nl.npy")
+
+        experiment_subfolders = glob.glob(experiment_root + "/seed*")
+        if len(experiment_subfolders) == 0:
+            print("No seed folders used, data must be at: {}".format(
+                experiment_root))
+            experiment_subfolders = [experiment_root]
+
+        for seed_folder in experiment_subfolders:
+            code_path = os.path.join(seed_folder, "train-code.npy")
+            nl_path = os.path.join(seed_folder, "train-nl.npy")
+
+            run_single_experiment(
+                seed_folder,
+                code_path,
+                nl_path,
+                valid_code_path,
+                valid_nl_path,
+                test_code_path,
+                test_nl_path,
+                embeddings_path,
+                encoder_path,
+                force=force,
+            )
+
+
+def run_single_experiment(
+        folder,
+        code_path,
+        nl_path,
+        valid_code_path,
+        valid_nl_path,
+        test_code_path,
+        test_nl_path,
+        embeddings_path,
+        encoder_path,
+        force=False,
+):
+    eval_results_path = os.path.join(folder, "results.json")
+    if os.path.exists(eval_results_path) and not force:
+        print("Skipping {}, results.json exists".format(folder))
+        print("Use --force if re-run is desired")
+        return
+
+        train(
+            code_path,
+            nl_path,
+            test_code_path,
+            test_nl_path,
+            embeddings_path,
+            encoder_path,
+            print_every=1000,
+            save_every=10,
+            output_folder=folder,
+            valid_code_path=valid_code_path,
+            valid_docstrings_path=valid_nl_path,
+        )
+        # load the best model based on validation loss
+        model_paths = glob.glob(os.path.join(folder, "models", "*best"))
+        assert len(model_paths) == 1, "Should only have 1 best model"
+        model = load_model(model_paths[0])
+
+        test_code, test_queries = load_evaluation_data(
+            test_code_path,
+            test_nl_path,
+        )
+
+        eval_results = evaluate_with_known_answer(
+            model,
+            test_code,
+            test_queries,
+        )
+        with open(eval_results_path, "w") as fout:
+            json.dump(eval_results, fout)
+
+
 def initial_experiments(output_dir):
     """Experiments used at the start to figure out what made sense to try"""
     # pipelines
@@ -446,139 +579,6 @@ def paper_experiments(output_dir):
     experiments.append(nl4)
 
     return experiments
-
-
-def generate_experiments(
-        train_data_path,
-        test_data_path,
-        output_dir,
-        train_downsample=None,
-        seed=None,
-        force=False,
-):
-    """
-    Generates subfolders for experiments
-    """
-    with open(train_data_path, "rb") as fin:
-        train_data = pickle.load(fin)
-
-    with open(test_data_path, "rb") as fin:
-        test_data = pickle.load(fin)
-
-    utils.create_dir(output_dir)
-
-    experiments = paper_experiments(output_dir)
-
-    for exp in experiments:
-        print("Generating experiment: {}".format(exp["output_dir"]))
-        if os.path.exists(exp["output_dir"]) and not force:
-            print("Skipping {}, output folder exists".format(
-                exp["output_dir"]))
-            print("Use --force if re-run is desired")
-            continue
-
-        generate_experiment_folder(
-            train_data,
-            test_data,
-            code_pipeline=exp["code"],
-            nl_pipeline=exp["nl"],
-            code_test_pipeline=exp["code_test"],
-            nl_test_pipeline=exp["nl_test"],
-            min_vocab_count=exp["min_count"],
-            output_dir=exp["output_dir"],
-            downsample_train=exp["downsample_train"],
-            downsample_valid=exp["downsample_valid"],
-            seeds=exp.get("seeds", seed),
-        )
-
-
-def run_experiments(base_dir, force=False):
-    experiment_folders = [
-        os.path.join(base_dir, p) for p in os.listdir(base_dir)
-    ]
-    for experiment_root in experiment_folders:
-        # Embeddings, encoder, test and validation data are seed independent
-        embeddings_path = os.path.join(experiment_root, "embeddings.vec")
-        encoder_path = os.path.join(experiment_root, "encoder.pkl")
-
-        test_code_path = os.path.join(experiment_root, "test-code.npy")
-        test_nl_path = os.path.join(experiment_root, "test-nl.npy")
-
-        valid_code_path = os.path.join(experiment_root, "valid-code.npy")
-        valid_nl_path = os.path.join(experiment_root, "valid-nl.npy")
-
-        experiment_subfolders = glob.glob(experiment_root + "/seed*")
-        if len(experiment_subfolders) == 0:
-            print("No seed folders used, data must be at: {}".format(
-                experiment_root))
-            experiment_subfolders = [experiment_root]
-
-        for seed_folder in experiment_subfolders:
-            code_path = os.path.join(seed_folder, "train-code.npy")
-            nl_path = os.path.join(seed_folder, "train-nl.npy")
-
-            run_single_experiment(
-                seed_folder,
-                code_path,
-                nl_path,
-                valid_code_path,
-                valid_nl_path,
-                test_code_path,
-                test_nl_path,
-                embeddings_path,
-                encoder_path,
-                force=force,
-            )
-
-
-def run_single_experiment(
-        folder,
-        code_path,
-        nl_path,
-        valid_code_path,
-        valid_nl_path,
-        test_code_path,
-        test_nl_path,
-        embeddings_path,
-        encoder_path,
-        force=False,
-):
-    eval_results_path = os.path.join(folder, "results.json")
-    if os.path.exists(eval_results_path) and not force:
-        print("Skipping {}, results.json exists".format(folder))
-        print("Use --force if re-run is desired")
-        return
-
-        train(
-            code_path,
-            nl_path,
-            test_code_path,
-            test_nl_path,
-            embeddings_path,
-            encoder_path,
-            print_every=1000,
-            save_every=10,
-            output_folder=folder,
-            valid_code_path=valid_code_path,
-            valid_docstrings_path=valid_nl_path,
-        )
-        # load the best model based on validation loss
-        model_paths = glob.glob(os.path.join(folder, "models", "*best"))
-        assert len(model_paths) == 1, "Should only have 1 best model"
-        model = load_model(model_paths[0])
-
-        test_code, test_queries = load_evaluation_data(
-            test_code_path,
-            test_nl_path,
-        )
-
-        eval_results = evaluate_with_known_answer(
-            model,
-            test_code,
-            test_queries,
-        )
-        with open(eval_results_path, "w") as fout:
-            json.dump(eval_results, fout)
 
 
 def get_args():
