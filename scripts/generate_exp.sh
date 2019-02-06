3d6c0f6b6d0bef1d834d003bfa77ebfdc70e017e
Jose Cambronero <jcamsan@mit.edu>
2019-02-06 16:44:46 +0000

    add generate/run bash scripts with everything we need

--- scripts/generate_exp.sh
+++ scripts/generate_exp.sh
@@ -1,8 +1,37 @@
 cd ..
+# # Code experiments
 python -m src.experiments generate \
 --train data/github/dataset.pkl  \
 --test_names conala github \
 --test_paths data/conala/dataset.pkl data/github/test-dataset.pkl \
 --output experiment_pipelines \
---subset nl-6 nl-7 \
---downsample 10000 --seed 42
+--downsample 10000 --seed 42 \
+--subset code-1 code-2 code-3 code-4 code-5 code-6
+
+# # NL experiments
+python -m src.experiments generate \
+--train data/github/dataset.pkl  \
+--test_names conala github \
+--test_paths data/conala/dataset.pkl data/github/test-dataset.pkl \
+--output experiment_pipelines \
+--downsample 10000 --seed 42 \
+--subset nl-1 nl-2 nl-3 nl-4 nl-5 nl-6 nl-7
+
+
+# Size experiments
+python -m src.experiments generate \
+--train data/github/dataset.pkl  \
+--test_names conala github \
+--test_paths data/conala/dataset.pkl data/github/test-dataset.pkl \
+--output experiment_pipelines \
+--downsample 10000 --seed 42 \
+--subset size-1 size-2 size-3 size-4 size-5 size-6 size-7
+
+# Full data set training
+python -m src.experiments generate \
+--train data/github/dataset.pkl  \
+--test_names conala github \
+--test_paths data/conala/dataset.pkl data/github/test-dataset.pkl \
+--output experiment_pipelines \
+--downsample 10000 --seed 42 \
+--subset full
--- scripts/run_exp.sh
+++ scripts/run_exp.sh
@@ -1,5 +1,34 @@
 cd ..
+## Fixed embeddings
+# Code experiments
 python -m src.experiments run \
  --data experiment_pipelines/ \
  --model all \
- --test all
+ --test all \
+ --subset code-1 code-2 code-3 code-4 code-5 code-6
+
+# NL experiments
+ python -m src.experiments run \
+  --data experiment_pipelines/ \
+  --model all \
+  --test all \
+  --subset nl-1 nl-2 nl-3 nl-4 nl-5 nl-6 nl-7
+
+
+# Size experiments
+python -m src.experiments run \
+ --data experiment_pipelines/ \
+ --model all \
+ --test all \
+ --subset size-1 size-2 size-3 size-4 size-5 size-6 size-7
+
+# Full dataset
+python -m src.experiments run \
+ --data experiment_pipelines/ \
+ --model all \
+ --test all \
+ --subset full
+
+
+ ## Tuned embeddings
+ # Add --tune to each of the calls above (note that it overwrites model folders)
